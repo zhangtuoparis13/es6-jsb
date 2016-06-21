804a4ee974dff4d3521f97308c5086d201d7dcef
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-06-21 10:30:48 +0200

    Initial commit

---
+++ ryf2.js
@@ -0,0 +1,28 @@
+/*
+* @Author: TBtuo
+* @Date:   2016-06-14 11:16:12
+* @Last Modified by:   TBtuo
+* @Last Modified time: 2016-06-14 11:52:45
+*/
+
+'use strict';
+
+/*let temp = new Date();
+
+function f() {
+  console.log(temp);
+  if (false) {
+    var temp = "hello world";
+  }
+}
+
+f();*/
+
+/*var s = 'zhangtuo';
+
+for (let i = 0; i < s.length; i++) {
+	console.log(s[i]);
+}
+console.log(i);
+*/
+
